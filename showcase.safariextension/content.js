dbb3ef2a611cb72bfdb49b710c43857591442efd
Mihaly Kristof <kristof.mihaly@nomo.hu>
2019-04-16 15:32:20 +0200

    init commit

---
+++ showcase.safariextension/content.js
@@ -0,0 +1,4 @@
+console.log("HEY IT LOADED");
+var test = document.body.appendChild(document.createElement("div"));
+test.className="hello";
+test.innerHTML = "This should have a <strong>green</strong> background";
